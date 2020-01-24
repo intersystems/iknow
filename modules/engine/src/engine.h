18f64fe76089d45a95d26264ed48f807ab4835be
Jos Denys <Jos.Denys@intersystems.com>
2020-01-24 10:04:11 -0500

    Engine interface documentation.
    Copied from Perforce
     Change: 4220340

--- modules/engine/src/engine.h
+++ modules/engine/src/engine.h
@@ -22,12 +22,14 @@
 #include "IkIndexInput.h"
 #include "IkIndexOutput.h"
 
+// iknowdata collects the output of the text indexer.
 namespace iknowdata {
-
+	// after indexing, a document is a list of sentence data, the latter is a list of "struct Entity" objects.
 	struct Entity
 	{
-		enum eType { NonRelevant = 0, Concept, Relation, PathRelevant }; // The 4 base entity base types
+		enum eType { NonRelevant = 0, Concept, Relation, PathRelevant }; // The 4 entity base types
 
+		// the constructors are invoked by the indexer, to store linguistic data, for the client to "read" 
 		Entity() : type_(NonRelevant) // The default one, for historical reasons
 		{}
 		Entity(eType type, size_t start, size_t stop, std::string& index) : type_(type), offset_start_(start), offset_stop_(stop), index_value_(index), entity_id_(NonRelevant)
@@ -37,22 +39,30 @@ namespace iknowdata {
 		Entity(eType type, size_t start, size_t stop, std::string& index, double dominance, size_t entity_id) : type_(type), offset_start_(start), offset_stop_(stop), index_value_(index), dominance_value_(dominance), entity_id_(entity_id)
 		{}
 
-		eType type_;
-		size_t offset_start_;
-		size_t offset_stop_;
+		eType type_;	// type of entity, see eType definition
+		size_t offset_start_;	// text offset of entity start position
+		size_t offset_stop_;	// text offset of entity stop position
+		// the index_value is the normalized form of the entity text
 		std::string /*const*/ index_value_; // utf8 encoded, g++ refuses 'const' because of needed copy constructor in vector
 		double dominance_value_;
 		size_t entity_id_; // if not concept, this value equals -1
 	};
 
+	// the definition of a sentence : a vector of entities
 	typedef std::vector<Entity> iKnow_Sentence;
+
+	// utility functions to retrieve the starting and end offset of a sentence
 	inline size_t sentence_offset_start(iKnow_Sentence& sentence) { return sentence.begin()->offset_start_; }
 	inline size_t sentence_offset_stop(iKnow_Sentence& sentence) { return (sentence.end() - 1)->offset_stop_; }
 
+	// the text source (document) after indexing : a vector of iKnow_Sentence objects
 	typedef std::vector<iKnow_Sentence> iKnow_Sentences;
+	// the definition of a path : a (reduced) vector of entities : non-relevants are filtered
 	typedef std::vector<Entity> iKnow_Path;
+	// the text document as a collection of iKnow_Path objects
 	typedef std::vector<iKnow_Path> iKnow_Paths;
-	typedef std::vector<std::vector<iknowdata::Entity> > iKnow_CRCs;
+	// the text document as a collection of a per sentence CRC list
+	typedef std::vector<std::vector<Entity> > iKnow_CRCs;
 }
 
 	struct Attribute
@@ -80,27 +90,37 @@ namespace iknowdata {
 class IKNOW_API iKnowEngine
 {
 public:
+	// Utility function : retrieves the set of supported languages : ("en", "de", "ru", "es", "fr", "ja", "nl", "pt", "sv", "uk", "cs")
 	static const std::set<std::string>& GetLanguagesSet(void);
 
 	enum errcodes {
 		iknow_language_not_supported = -1 // unsupported language
 	};
+	// Constructor and destructor
 	iKnowEngine();
 	~iKnowEngine();
 	
+	// The main indexing function :
+	//	- text_source : the text input for indexing, must be Unicode 2byte (UCS2) encoded.
+	//	- the language parameter, see supported languages
+	// Current limitations : 
+	//	- works synchronous : the complete text_source is indexed, after return, use iKnowEngine methods to retrieve indexing information.
+	//	- works single threaded : a mutex protects multithread functioning. Use multiprocess to bypass this (current) limitation.
 	void index(iknow::base::String& text_source, const std::string& language);
+
+	// Adds User Dictionary annotations for customizing purposes
 	void addUdctAnnotation(size_t start, size_t stop, const char* UdctLabel) {
 		// std::cout << start << ":" << stop << ":" << UdctLabel << std::endl;
 		m_map_udct_annotations.insert(std::make_pair(start, iknow::core::IkIndexInput::IknowAnnotation(start, stop, UdctLabel)));
 	}
-
-	iknowdata::iKnow_Sentences m_sentences;
+	iknowdata::iKnow_Sentences& sentences() { return m_sentences; }
 	iknowdata::iKnow_Paths m_paths;
 	iknowdata::iKnow_CRCs m_crcs;
 	iKnow_Attributes m_attributes;
 	iknow::core::IkConceptProximity::ProximityPairVector_t m_proximity;
 
 private:
+	iknowdata::iKnow_Sentences m_sentences;
 	iknow::core::IkIndexInput::mapInputAnnotations_t m_map_udct_annotations;
 
 };
--- modules/enginetest/enginetest.cpp
+++ modules/enginetest/enginetest.cpp
@@ -10,9 +10,11 @@
 using iknow::base::String;
 using iknow::base::IkStringEncoding;
 
+// Utility function to convert utf8 text input into ucs2 (unicode 2byte)
 inline String ucs2(const char* input_text) {
 	return String(IkStringEncoding::UTF8ToBase(input_text));
 }
+// Based on the language code, choose a sample text source
 String getSampleText(std::string language_code) { // must be ucs-2 encoded text.
 	if (language_code == "en") return ucs2("Be the change you want to see in life.");
 	if (language_code == "de") return ucs2("Oder die Erkundung der Natur - und zwar ohne Anleitung.");
@@ -32,30 +34,30 @@ String getSampleText(std::string language_code) { // must be ucs-2 encoded text.
 int main(int argc, char* argv[])
 {
 	const char *default_lang = "en";
-	const std::set<std::string> languages_set = iKnowEngine::GetLanguagesSet();
+	const std::set<std::string> languages_set = iKnowEngine::GetLanguagesSet(); // get the set of supported languages
 
 	try {
 		std::cout << "iKnowEngine driver" << std::endl << std::endl;
-		std::ofstream os("iknowdata.log");
+		std::ofstream os("iknowdata.log"); // create a utf8 logfile for collecting indexing data
 		os << "\xEF\xBB\xBF"; // Force utf8 header, maybe utf8 is not the system codepage
 
-		iKnowEngine myEngine;
-		for (std::set<std::string>::iterator it_lang = languages_set.begin(); it_lang != languages_set.end(); ++it_lang) {
+		iKnowEngine myEngine; // construct an engine object
+		for (std::set<std::string>::iterator it_lang = languages_set.begin(); it_lang != languages_set.end(); ++it_lang) { // iterate over all languages
 			if (languages_set.find(*it_lang) == languages_set.end()) return iKnowEngine::iknow_language_not_supported;
-			String text_source = getSampleText(*it_lang);
+			String text_source = getSampleText(*it_lang); // get a language corresponding text sample, in ucs2 
 			std::cout << "Indexing " << *it_lang << std::endl;
-			myEngine.index(text_source, *it_lang);
+			myEngine.index(text_source, *it_lang);	// index the text source
 
 			using namespace iknowdata;
-			for (iKnow_Sentences::iterator it = myEngine.m_sentences.begin(); it != myEngine.m_sentences.end(); ++it)
+			for (iKnow_Sentences::iterator it = myEngine.sentences().begin(); it != myEngine.sentences().end(); ++it) // iterate over the sentences
 			{
-				iKnow_Sentence& sent = *it; 
+				iKnow_Sentence& sent = *it; // retrieve a sentence reference 
 
 				// start and stop offset of the sentence
 				std::cout << "S:" << sentence_offset_start(sent) << ":" << sentence_offset_stop(sent) << ";" << std::endl;
 
 				// sentence entity sequence, per 4 base types : concept, relation, path-relevant, non-relevant.
-				for (iKnow_Sentence::iterator it_entity = sent.begin(); it_entity != sent.end(); ++it_entity) {
+				for (iKnow_Sentence::iterator it_entity = sent.begin(); it_entity != sent.end(); ++it_entity) { // iterate over the sentence entities
 					char t = it_entity->type_ == Entity::Concept ? 'c' : (it_entity->type_ == Entity::Relation ? 'r' : (it_entity->type_ == Entity::PathRelevant ? 'p' : 'n'));
 					std::cout << t;
 					os << "<" << t << ">" << it_entity->index_value_ << "</" << t << ">";
