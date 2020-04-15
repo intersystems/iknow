48ce97a2c004a45eb60af16a6f4d0397832937e2
Aohan Dang <aohan.dang@intersystems.com>
2020-04-15 15:24:24 -0400

    ensure clang++ is used to build iknowpy on Mac OS

--- modules/iknowpy/setup.py
+++ modules/iknowpy/setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
@@ -8,14 +9,21 @@ if 'ICUDIR' in os.environ:
 else:
 	icudir = '../../thirdparty/icu'
 
-if os.name == 'nt':
+if sys.platform == 'win32':
 	library_dirs = ['../../kit/x64/Release/bin']
+	extra_compile_args = []
 else:
 	if 'IKNOWPLAT' in os.environ:
 		iknowplat = os.environ['IKNOWPLAT']
 	else:
 		raise RuntimeError("'IKNOWPLAT' is not defined")
 	library_dirs = ['../../kit/{}/release/bin'.format(iknowplat)]
+
+	if sys.platform == 'darwin':
+		os.environ['CC'] = 'clang++'
+		extra_compile_args = ['-std=c++11']
+	else:
+		extra_compile_args = []
 
 setup(
 	ext_modules=cythonize(
@@ -25,6 +33,7 @@ setup(
 			include_dirs=['../engine/src', '../core/src/headers', '../base/src/headers', os.path.join(icudir, 'include')],
 			library_dirs=library_dirs,
 			libraries=['iknowengine'],
+			extra_compile_args=extra_compile_args
 		)],
 		compiler_directives={'language_level': '3'}
 	)
