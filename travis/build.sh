d8c3adba0759f967f9e5962d1c81680d1ee299ab
Aohan Dang <aohan.dang@intersystems.com>
2020-06-16 15:27:20 -0400

    try to add Mac OS, attempt 3

---
+++ travis/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Run the correct build script for the current platform.
+
+set -euxo pipefail
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  ${WAIT-} docker run --rm -e TRAVIS_COMMIT -v $(pwd):/iknow $DOCKER_IMAGE $PRE_CMD /iknow/travis/build_manylinux.sh $TAG $ICU_URL $PYPI_TOKEN
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  travis/build_osx.sh
+fi
---
+++ travis/build_osx.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+echo "This is Mac OS X"
