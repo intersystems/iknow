3358f4391b8c9548f19c1f81b09d6840cdd9e1c0
Aohan Dang <aohan.dang@intersystems.com>
2020-06-17 13:17:32 -0400

    use standalone Python installer instead of web-based installer

--- travis/install_windows.sh
+++ travis/install_windows.sh
@@ -8,10 +8,10 @@ set -euxo pipefail
 choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 
 # Python
-wget https://www.python.org/ftp/python/3.5.4/python-3.5.4-amd64-webinstall.exe
-wget https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64-webinstall.exe
-wget https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64-webinstall.exe
-wget https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64-webinstall.exe
+wget https://www.python.org/ftp/python/3.5.4/python-3.5.4-amd64.exe
+wget https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
+wget https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64.exe
+wget https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64.exe
 
 for PYINSTALL in python-*.exe; do
   "./$PYINSTALL" /passive InstallAllUsers=1 Shortcuts=0 Include_doc=0 Include_launcher=0 Include_tcltk=0 Include_test=0
