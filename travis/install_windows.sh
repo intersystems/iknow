d9edb9205df8a3018397ab12d39577742848cafd
Aohan Dang <aohan.dang@intersystems.com>
2020-06-17 12:32:32 -0400

    update Windows install

--- travis/install_windows.sh
+++ travis/install_windows.sh
@@ -6,7 +6,21 @@
 choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 
 # Python
-choco install python --version=3.5.4 -my
-choco install python --version=3.6.8 -my
-choco install python --version=3.7.7 -my
-choco install python --version=3.8.2 -my
+wget https://www.python.org/ftp/python/3.5.4/python-3.5.4-amd64-webinstall.exe
+wget https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64-webinstall.exe
+wget https://www.python.org/ftp/python/3.7.7/python-3.7.7-amd64-webinstall.exe
+wget https://www.python.org/ftp/python/3.8.3/python-3.8.3-amd64-webinstall.exe
+
+for PYINSTALL in python-*.exe; do
+  "$PYINSTALL" /passive InstallAllUsers=1 Shortcuts=0 Include_doc=0 Include_launcher=0 Include_tcltk=0 Include_test=0
+done
+
+# Python packages
+for PYTHON in /c/"Program Files"/Python3*/python.exe; do
+  "$PYTHON" -m pip install -U pip
+  PACKAGES="cython setuptools wheel"
+  if [[ "$PYTHON" == *"Python38"* ]]; then
+    PACKAGES="$PACKAGES twine"
+  fi
+  "$PYTHON" -m pip install -U $PACKAGES
+done
