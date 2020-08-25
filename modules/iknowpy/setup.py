0559ca0d9cf47ebb8ac63c99dc6671ae0700b990
Aohan Dang <aohan.dang@intersystems.com>
2020-08-25 13:21:58 -0400

    add Python 3.9 to package trove classifers

--- modules/iknowpy/setup.py
+++ modules/iknowpy/setup.py
@@ -498,6 +498,7 @@ try:
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: Implementation :: CPython',
         ],
         keywords='NLP',
