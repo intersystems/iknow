6ac41c1c185080b6c74b022146066f817840c404
Aohan Dang <aohan.dang@intersystems.com>
2020-09-28 13:43:39 -0400

    reduce verbosity of installs

--- travis/install_windows.sh
+++ travis/install_windows.sh
@@ -5,19 +5,17 @@
 set -euxo pipefail
 
 # Visual Studio 2019
-choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
+choco install visualstudio2019buildtools --limit-output --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --quiet"
 
 # buildcache
 wget -nv https://github.com/mbitsnbites/buildcache/releases/latest/download/buildcache-win-msvc.zip
 unzip -qj buildcache-win-msvc.zip
 
 # Python
-choco install nuget.commandline
-nuget.exe install python -Version 3.5.4
-nuget.exe install python -Version 3.6.8
-nuget.exe install python -Version 3.7.8
-nuget.exe install python -Version 3.8.6
-nuget.exe install python -Version 3.9.0-rc2
+choco install nuget.commandline --limit-output 
+for PYVERSION in 3.5.4 3.6.8 3.7.8 3.8.6 3.9.0-rc2; do
+  nuget.exe install python -Version "$PYVERSION" -NonInteractive -Verbosity quiet
+done
 
 # Python packages
 for PYTHON in python.*/tools/python.exe; do
