e2e696c8b40b8070727631fb9d9d16ed60ee2331
isc-adang <aohan.dang@intersystems.com>
2020-12-09 13:51:16 -0500

    provide useful error message when iknowpy is accidentally imported from source directory

--- modules/iknowpy/iknowpy/__init__.py
+++ modules/iknowpy/iknowpy/__init__.py
@@ -1,3 +1,21 @@
+# provide useful error message when accidentally imported from source directory
+import os
+import inspect
+file_directory = os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda: 0)))
+if os.path.isfile(os.path.join(file_directory, 'SOURCE')):
+    raise ImportError(
+        'You have imported the source package {} instead of the installed '
+        'package, which is not allowed. This occurred because the `iknowpy\' '
+        'package source is in the directory where the import occurred and took '
+        'precedence over the installed package. If you tried importing '
+        '`iknowpy\' from the Python interactive console, change your working '
+        'directory and try again. If you tried importing `iknowpy\' within a '
+        'Python script, move the script to a different directory.'
+        .format(file_directory)
+    )
+del os, inspect, file_directory
+
+# export public variables and classes
 from .version import __version__
 from .labels import Labels
 from .engine import iKnowEngine, UserDictionary
