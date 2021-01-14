fa4b5d2248966d889e63a9cc6d7b08072afc7d49
jdenys <jos.denys@intersystems.com>
2021-01-14 16:00:32 +0100

    Error check the 3 steps in language updating : - Language compiler - C++ building the dll/so modules - Unit test the engine

--- modules/lang_update.bat
+++ modules/lang_update.bat
@@ -6,8 +6,16 @@ if %errorlevel% NEQ 0 (
 	echo Failure in language compilation : Err=%errorlevel%
 	goto exit
 )
-MSBuild ..\..\..\..\modules\iKnowEngine.sln -p:Configuration="Release" -p:Platform="x64" -maxcpucount
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild" ..\..\..\..\modules\iKnowEngine.sln -p:Configuration="Release" -p:Platform="x64" -maxcpucount
+if %errorlevel% NEQ 0 (
+	echo Failure in building the iKnow engine : Err=%errorlevel%
+	goto exit
+)
 iKnowEngineTest
+if %errorlevel% NEQ 0 (
+	echo Failure in testing the iKnow engine : Err=%errorlevel%
+	goto exit
+)
 pushd ..\..\..\..\modules\iknowpy
 python setup.py install --fast
 popd
