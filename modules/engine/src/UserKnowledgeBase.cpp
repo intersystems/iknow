f0c6f72e7d9b3f02804b9d9ffc46516df22e9e02
jdenys <jos.denys@intersystems.com>
2021-08-23 15:38:35 +0200

    Enlarge internal User Dictionary buffer
    Allows for much larger user dictionaries.

--- modules/engine/src/UserKnowledgeBase.cpp
+++ modules/engine/src/UserKnowledgeBase.cpp
@@ -224,7 +224,7 @@ void LoadKbRangeAsTable(IterT begin, IterT end, size_t size, TransformerT& trans
 	table = allocator.Insert(table_builder.Build(allocator));
 }
 
-static const size_t kRawSize = 20*1024; // User dictionary is limited in size, 20k should suffice
+static const size_t kRawSize = 120*1024; // User dictionary is limited in size, 120k should suffice
 unsigned char* data_buffer(bool b_init)
 {
 	static unsigned char udct_memory[kRawSize]; // raw representation of user dictionary data, only allocated when used.
