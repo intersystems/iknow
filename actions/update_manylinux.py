bdc70973012623cc9b6f7063854c9af85942257d
isc-adang <aohan.dang@intersystems.com>
2022-03-28 13:49:07 -0400

    Fix manylinux autoupdater
    Quay seems to have removed the /image endpoint from its API. We have switched to the /tag endpoint, which provides the same information.

--- actions/update_manylinux.py
+++ actions/update_manylinux.py
@@ -15,19 +15,21 @@ manylinux2014_ppc64le_tag = vars['MANYLINUX2014_PPC64LE_TAG']
 # send Quay API requests and parse responses
 tags = []
 for repo in ('manylinux2014_x86_64', 'manylinux2014_aarch64', 'manylinux2014_ppc64le'):
-    r = requests.get(f'https://quay.io/api/v1/repository/pypa/{repo}/image')
+    r = requests.get(f'https://quay.io/api/v1/repository/pypa/{repo}/tag')
     json_data = r.json()
-    for image in reversed(json_data['images']):
-        if 'latest' in image['tags']:
+    for tag_info in json_data['tags']:
+        if tag_info['name'] == 'latest':
+            manifest_digest = tag_info['manifest_digest']
             break
     else:
-        raise ValueError(f'latest tag not found: {json_data["images"]}')
-    for tag in image['tags']:
-        if tag != 'latest':
+        raise ValueError(f'latest tag not found: {json_data["tags"]}')
+    for tag_info in json_data['tags']:
+        if tag_info['manifest_digest'] == manifest_digest and tag_info['name'] != 'latest':
+            tag_name = tag_info['name']
             break
     else:
-        raise ValueError(f'failed to find tag for latest image: {image}')
-    tags.append(tag)
+        raise ValueError(f'failed to find tag for latest image: {json_data["tags"]}')
+    tags.append(tag_name)
 
 # set variables to latest ICU version
 vars['MANYLINUX2014_X86_64_TAG'] = tags[0]
