ec7ef7035e2b4e81373b84b5b290addd07e7ba6b
jdenys <jos.denys@intersystems.com>
2022-04-05 15:55:36 +0200

    Implement Issue#236

--- modules/compiler/iKnowLanguageCompiler/iKnow_KB_Label.h
+++ modules/compiler/iKnowLanguageCompiler/iKnow_KB_Label.h
@@ -2,10 +2,62 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <set>
 
 namespace iknow {
 	namespace csvdata {
 		class CSV_DataGenerator; // forward to avoid circular include
+		static const std::set<std::string> accepted_types = { // label types
+			"typeConcept",
+			"typeOther",
+			"typeEndConcept",
+			"typeRelation",
+			"typeAttribute",
+			"typeNonRelevant",
+			"typeAmbiguous",
+			"typePathRelevant",
+			"typeBeginEndConcept",
+			"typeBeginConcept",
+			"typeBeginRelation",
+			"typeEndRelation",
+			"typeBeginEndRelation",
+			"typeLiteral"
+		};
+		static const std::set<std::string> accepted_attributes = {
+			"Entity(Certainty)",
+			"Entity(Generic1)",
+			"Path(Begin,Generic1)",
+			"Path(End,Generic1)",
+			"Entity(Generic2)",
+			"Path(Begin,Generic2)",
+			"Path(End,Generic2)",
+			"Entity(Generic3)",
+			"Path(Begin,Generic3)",
+			"Path(End,Generic3)",
+			"Entity(Negation)",
+			"Path(Begin,Negation)",
+			"Path(End,Negation)",
+			"Entity(DateTime)",
+			"Entity(PositiveSentiment)",
+			"Entity(NegativeSentiment)",
+			"Path(Begin,PositiveSentiment)",
+			"Path(End,PositiveSentiment)",
+			"Path(Begin,NegativeSentiment)",
+			"Path(End,NegativeSentiment)",
+			"Entity(Measurement,Value,Unit)",
+			"Entity(Frequency)",
+			"Entity(Duration)",
+			"Path(Begin,DateTime)",
+			"Path(End,DateTime)",
+			"Path(Begin,Measurement)",
+			"Path(End,Measurement)",
+			"Entity(Measurement)",
+			"Entity(Measurement,Unit)",
+			"Entity(Measurement,Value)",
+			"Path(Begin,Certainty)",
+			"Path(End,Certainty)"
+
+		};
 		static const std::vector<std::string> special_labels = { // language independent labels
 			";1,$;Concept;typeConcept;;0;",
 			";1,$;Join;typeOther;;0;",
@@ -54,10 +106,15 @@ namespace iknow {
 
 			iKnow_KB_Label(std::string name, std::string type) :
 				Name(name), Type(type)
-			{}
+			{
+				_checktype(type.c_str());
+			}
 			iKnow_KB_Label(const char* name, const char* type, const char* attributes) :
 				Name(name), Type(type), Attributes(attributes)
-			{}
+			{
+				_checktype(type);
+				_checkattribute(attributes);
+			}
 			std::string Name; // Name As %String(MAXLEN = 256, XMLPROJECTION = "ATTRIBUTE")[Required];
 			std::string Type; // Type As %String(MAXLEN = 256, XMLPROJECTION = "ATTRIBUTE")[Required];
 			std::string Attributes; // Attributes As %String(MAXLEN = 256, XMLPROJECTION = "ATTRIBUTE");
@@ -67,6 +124,24 @@ namespace iknow {
 		private:
 			static void LoadSpecialLabels(CSV_DataGenerator& kb);
 			static iKnow_KB_Label LabelFromString(std::vector<std::string>& row_label, std::string &isDefault);
+			static void _checktype(const char* type) {
+				if (accepted_types.count(std::string(type)) == 0) { // type unknown
+					throw ExceptionFrom<iKnow_KB_Label>("Label Type: \"" + std::string(type) + "\" Unknown !");
+				}
+				return;
+			}
+			static void _checkattribute(const char* attribute) {
+				if (accepted_attributes.count(std::string(attribute)) == 0) {
+					// EVSlot(0, 0, Topic, L, B) | EVSlot(0, 1, Subject, L, B) | EVSlot(0, 2, Object, L, B)
+					if (std::string(attribute).find("EVSlot") != std::string::npos)
+						return;
+					// EVValue(0, 1, Subject, L, B)
+					if (std::string(attribute).find("EVValue") != std::string::npos)
+						return;
+					throw ExceptionFrom<iKnow_KB_Label>("Attribute: \"" + std::string(attribute) + "\" Unknown !");
+				}
+				return;
+			}
 		};
 	}
 }
