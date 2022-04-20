2fe40aa583b4cfde2fee984b0533d91d77baaa8d
isc-adang <aohan.dang@intersystems.com>
2022-04-20 10:10:42 -0400

    Display error if git command fails

--- modules/iknowpy/setup.py
+++ modules/iknowpy/setup.py
@@ -781,11 +781,18 @@ try:
 
         # include git revision in distribution
         with open('iknowpy/git_revision', 'w') as f:
-            subprocess.run(
-                ['git', 'rev-parse', 'HEAD'],
-                stdout=f, stderr=subprocess.PIPE, universal_newlines=True,
-                check=True
-            )
+            try:
+                subprocess.run(
+                    ['git', 'rev-parse', 'HEAD'],
+                    stdout=f, stderr=subprocess.PIPE, universal_newlines=True,
+                    check=True
+                )
+            except subprocess.CalledProcessError as ex:
+                if ex.stdout:
+                    print(ex.stdout)
+                if ex.stderr:
+                    print(ex.stderr)
+                raise
         package_data = {'iknowpy': ['git_revision']}
     setup(
         name='iknowpy',
