a65613268b8d7bedd091c8bba6ea80b18c5b5b5d
isc-adang <aohan.dang@intersystems.com>
2022-10-25 12:50:21 -0400

    Delete caches associated with closed pull requests and deleted branches

--- actions/clean_cache.py
+++ actions/clean_cache.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-"""Delete all but the most recent single-use caches. This keeps the cache clear
-of items that we know we will never use again, reducing the chance that GitHub
-Actions will evict items that we still need.
+"""Delete cache items that will never be used again, including the following
+items.
+- All but the most recent single-use cache per ref
+- Caches associated with closed pull requests
+- Caches associated with deleted branches
+This reduces the chance that GitHub Actions will evict items that we still
+need.
 
 Usage: clean_cache.py GITHUB_TOKEN"""
 
+import functools
 import itertools
 import os
 import re
@@ -14,36 +19,64 @@ import requests
 
 
 def cache_sort_key(cache: dict):
-    match = re.match(r'(.*-run-)(\d+)', cache['key'])
+    match = re.fullmatch(r'(.*-run-)(\d+)', cache['key'])
     return cache['ref'], match[1], int(match[2])
 
 
 def cache_group_key(cache: dict):
-    match = re.match(r'(.*-run-)\d+', cache['key'])
+    match = re.fullmatch(r'(.*-run-)\d+', cache['key'])
     return cache['ref'], match[1]
 
 
-headers = {'Accept': 'application/vnd.github+json', 'Authorization': 'Bearer ' + sys.argv[1]}
-caches = []
+@functools.lru_cache(maxsize=None)
+def get(url: str):
+    """Wrapper for requests.get() that caches results and uses HEADERS"""
+    return requests.get(url, headers=HEADERS)
+
+
+HEADERS = {'Accept': 'application/vnd.github+json', 'Authorization': 'Bearer ' + sys.argv[1]}
+
+run_caches = []
 page = 1
 total_count = 0
+to_delete = []
 while True:
-    json_data = requests.get(f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/actions/caches?per_page=100&page={page}', headers=headers).json()
+    json_data = requests.get(f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/actions/caches?per_page=100&page={page}', headers=HEADERS).json()
     new_caches = json_data['actions_caches']
-    caches.extend(filter(lambda cache: re.match(r'.*-run-\d+', cache['key']), new_caches))
+    for cache in new_caches:
+        if match := re.fullmatch(r'refs/pull/(\d+)/merge', cache['ref']):
+            # find caches associated with closed pull requests
+            req = get(f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/pulls/{match[1]}')
+            if req.status_code == 200:
+                if req.json()['state'] == 'closed':
+                    to_delete.append(cache)
+            else:
+                print(f'Failed to obtain pull request information for cache: {cache}')
+        elif match := re.fullmatch('refs/heads/(.+)', cache['ref']):
+            # find caches associated with deleted branches
+            req = get(f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/branches/{match[1]}')
+            if req.status_code == 404:
+                to_delete.append(cache)
+            elif req.status_code != 200:
+                print(f'Failed to obtain branch information for cache: {cache}')
+        if re.fullmatch(r'.*-run-\d+', cache['key']):
+            run_caches.append(cache)
     if not new_caches or (total_count := total_count + len(new_caches)) >= json_data['total_count']:
         break
     page += 1
 
-caches.sort(key=cache_sort_key)
-deleted = False
-for _, group in itertools.groupby(caches, cache_group_key):
+run_caches.sort(key=cache_sort_key)
+for _, group in itertools.groupby(run_caches, cache_group_key):
     for cache in list(group)[:-1]:
-        req = requests.delete(f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/actions/caches/{cache["id"]}', headers=headers)
-        if req.status_code == 204:
-            print('Deleted cache', cache)
-            deleted = True
-        else:
-            print('Failed to delete cache', cache)
-if not deleted:
-    print('No caches were deleted')
+        if cache not in to_delete:
+            to_delete.append(cache)
+
+size = 0
+for cache in to_delete:
+    req = requests.delete(f'https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/actions/caches/{cache["id"]}', headers=HEADERS)
+    if req.status_code == 204:
+        print('Deleted cache', cache)
+        size += cache['size_in_bytes']
+    else:
+        print('Failed to delete cache', cache)
+print(f'Freed {size:,} bytes')
